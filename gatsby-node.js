02bdba7146f07ac56dacb1d633af90e8987485df
tony dugué <duguetony@gmail.com>
2022-07-05 00:49:41 +0200

    Archive => generate pages & categories query & create template wit hero and breadcrumb

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,8 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   const { createPage } = actions
 
+  const archiveTemplate = path.resolve("./src/templates/archive.js")
+
   const result = await graphql(`
     {
       wp {
@@ -32,5 +34,38 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   }
 
   const { wp, allWpCategory} = result.data
-  console.log(wp, allWpCategory)
+
+  // Create pages for each category
+  allWpCategory.edges.forEach( category => {
+
+    const postsPerPage = wp.readingSettings.postsPerPage
+    const numberOfPosts = category.node.count
+    const numPages = Math.ceil(numberOfPosts / postsPerPage)
+
+    // Some categories may be empty and we don't want to create pages for them
+    // Also, don't want to create pages for uncategorized
+    if (numberOfPosts > 0 || category.node.name !== "uncategorized") {
+      Array.from({ length: numPages }).forEach( (_, i) => {
+        createPage({
+          path:
+            i === 0 ? category.node.uri : `${category.node.uri}${i + 1}`,
+          component: archiveTemplate,
+          context: {
+            limit: postsPerPage,
+            skip: i * postsPerPage,
+            numPages,
+            currentPage: i + 1,
+            catId: category.node.id,
+            catName: category.node.name,
+            catUri: category.node.uri,
+            categories: allWpCategory
+          }
+        })
+      })
+    }
+  })
 }
+
+
+
+
